231d61012e2cb69970f30040c6dbb6f970e1e44b
Franz-Eric Sill <107982913+qwerdenkerXD@users.noreply.github.com>
2023-04-20 13:04:57 +0200

    more documentation

--- SL.py
+++ SL.py
@@ -1,5 +1,7 @@
 """
 A module to practice clustering.
+
+Instead of using modules for WPGMA and dendogram plot, I implemented them by myself for more practice and, well, Python is fun :)
 """
 
 from matplotlib import pyplot as plot
@@ -155,6 +157,9 @@ def WPGMA(corrMat: dict) -> "(Newick string, length tree)":
 
 
 def plotHeatmap(plot, data, dataLabels=[]) -> None:
+    """
+    dataLabels: because the measurements are stored in arrays, there are labels needed for each index, so here the genes
+    """
     plot.set_xticks(ticks=np.arange(len(data)), labels=data.keys(), rotation=90, fontsize=15)
     plot.set_yticks(ticks=np.arange(GENES), labels=dataLabels, fontsize=15)
     vals = np.array(list(data.values()))
@@ -181,8 +186,14 @@ def treeNewick(newick: str, plot, root_loc="left", leafLabels=True, showScale=Tr
 
     def recursive(newick: str, plot, x=0, **kwargs) -> "float[tree depth]":
         """
-        Plots a dendogram recursively from a given newick string.
+        Plots a dendogram recursively from a given newick string, root is left.
+        This is done in 3 steps:
+        1. reach child-level: There are being the child labels plotted and their horizontal branches drawn
+        loop till end:
+            2. connection: horizontal branches of the next level are drawn recursively, so here will they be connected by the returned y-positions of these branches
+            3. root-drawing: if two branches were connected horizontally, there is the root branch following, fitting to the middle of the vertical connection branch
 
+        x: the x-position where to plot the root
         kwargs: treeDepth=0, lowestLeaf=x, leafLabels=True, nodeLabels=True, treeColor="black"
         """
         if newick[0] == "(" and newick[-1] == ")":
@@ -190,7 +201,7 @@ def treeNewick(newick: str, plot, root_loc="left", leafLabels=True, showScale=Tr
 
         lowestLeaf = kwargs.get("lowestLeaf", 15)  # important to have the same distance between the children/leafs
         treeColor = kwargs.get("treeColor", "black")
-        nodeLabels = kwargs.get("nodeLabels", True)
+        nodeLabels = kwargs.get("nodeLabels", True)  # not used for this script, but can be implemented
         leafLabels = kwargs.get("leafLabels", True)
         kwargs["treeDepth"] = kwargs.get("treeDepth", 0)
         myPosY = None  # necessary to return it to center the previous root properly
@@ -198,35 +209,37 @@ def treeNewick(newick: str, plot, root_loc="left", leafLabels=True, showScale=Tr
         if newick[0] == "(":
 
             # detect end of the subtree by finding its closing bracket
-            splitIndex, openBrackets = 1, 1
+            splitIndex, openBrackets = 1, 1  # splitIndex is one behind the character
             while openBrackets > 0:
                 openBrackets += newick[splitIndex] == "("
                 openBrackets -= newick[splitIndex] == ")"
                 splitIndex += 1
-            splitIndex += newick[splitIndex:].find(",")
+            splitIndex += newick[splitIndex:].find(",")  # if there is a subtree following like in (subtree)A:1,(subtree)B:1 then the following comma is the end
 
-            if newick[splitIndex] == ")":  # if it's sth. like (subTree)C:1
+            # splitIndex is now the index of the comma after the tree or (if no comma there) the last bracket's index
+
+            if newick[splitIndex] == ")":  # if it's sth. like (subTree)C:1            -> root-drawing (horizontal)
                 label, length = newick[splitIndex+1:].split(":")
                 kwargs["treeDepth"], kwargs["lowestLeaf"], myPosY = recursive(newick[1: splitIndex], plot, x + float(length), **kwargs)
                 # if nodeLabels:
                 #    plot.text(x + float(length), myPosY, " " + label)
                 draw([x, x + float(length)], [myPosY, myPosY], treeColor=treeColor)
 
-            else:  # if it's sth. like (subTree)R:1,(subTree)Q:1
+            else:  # if it's sth. like (subTree)R:1,(subTree)Q:1                       -> connection (vertical)
                 upper, lower = newick[:splitIndex], newick[splitIndex+1:]
                 kwargs["treeDepth"], kwargs["lowestLeaf"], upperPos = recursive(upper, plot, x, **kwargs)
                 kwargs["treeDepth"], kwargs["lowestLeaf"], lowerPos = recursive(lower, plot, x, **kwargs)
                 draw([x, x], [upperPos, lowerPos], treeColor=treeColor)
-                myPosY = (upperPos - lowerPos) / 2 + lowerPos
+                myPosY = (upperPos - lowerPos) / 2 + lowerPos  # centering for the previous root-line
 
-        elif "," in newick:  # if it's sth. like A:1,(subtree)B:1
+        elif "," in newick:  # if it's sth. like A:1,(subtree)B:1                       -> connection (vertical)
             upper, lower = newick.split(",", 1)
             kwargs["treeDepth"], kwargs["lowestLeaf"], upperPos = recursive(upper, plot, x, **kwargs)
             kwargs["treeDepth"], kwargs["lowestLeaf"], lowerPos = recursive(lower, plot, x, **kwargs)
             draw([x, x], [upperPos, lowerPos], treeColor=treeColor)
             myPosY = (upperPos - lowerPos) / 2 + lowerPos
 
-        elif newick:  # if it's sth. like A:1
+        elif newick:  # if it's sth. like A:1  # will only be reached if A:1 is a leaf  -> child-level (horizontal)
             label, length = newick.split(":")
             myPosY = lowestLeaf-1
             kwargs["treeDepth"] = max(kwargs.get("treeDepth", 0), x + float(length))
@@ -263,21 +276,29 @@ def treeNewick(newick: str, plot, root_loc="left", leafLabels=True, showScale=Tr
         raise ValueError("Newick string doesn't end with a semicolon")
 
     depth, _, _ = recursive(newick[:-1], plot, leafLabels=leafLabels)
+
     plot.spines['bottom'].set_visible(False)
     plot.spines['top'].set_visible(False)
     plot.spines['left'].set_visible(False)
     plot.spines['right'].set_visible(False)
+
     plot.set_xlim([0, depth])
     plot.set_xticks([0, round(depth/2, 2),  round(depth, 2)])
+
     plot.yaxis.set_visible(False)
     plot.xaxis.set_visible(False)
+
     if showScale:
         plot.xaxis.set_visible(True)
         plot.spines['bottom'].set_visible(True)
+
     allLabels = [text.get_text()[1:-1] for text in plot.texts]
+
+    # the following mirrors the plot to locate the root correctly
     if root_loc == "right":
         plot.set_xticklabels([round(depth, 2),  round(depth/2, 2), 0])
         plot.invert_xaxis()
+
     elif root_loc != "left":
         allLabels = allLabels[::-1]
         if showScale:
@@ -288,6 +309,7 @@ def treeNewick(newick: str, plot, root_loc="left", leafLabels=True, showScale=Tr
         plot.autoscale("x")
         plot.set_ylim([0, depth])
         plot.set_yticks([0, round(depth/2, 2),  round(depth, 2)])
+
         if root_loc == "top":
             plot.set_yticklabels([round(depth, 2),  round(depth/2, 2), 0])
             plot.invert_yaxis()
